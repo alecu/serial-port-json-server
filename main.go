572fa3038f37edc74bee4a433d036be90915b21a
Alejandro J. Cura <alecu@protocultura.net>
2015-10-12 21:17:49 -0300

    Serve content from html/ folder

--- main.go
+++ main.go
@@ -17,7 +17,6 @@ import (
 	//"runtime"
 	"io"
 	"runtime/debug"
-	"text/template"
 	"time"
 )
 
@@ -61,10 +60,6 @@ func defaultAssetPath() string {
 		return "."
 	}
 	return p.Dir
-}
-
-func homeHandler(c http.ResponseWriter, req *http.Request) {
-	homeTemplate.Execute(c, req.Host)
 }
 
 func launchSelfLater() {
@@ -164,7 +159,8 @@ func main() {
 	// launch our dummy data routine
 	//go d.run()
 
-	http.HandleFunc("/", homeHandler)
+    http.Handle("/html/", http.StripPrefix("/html/", http.FileServer(http.Dir("./html"))))
+    http.Handle("/", http.RedirectHandler("/html/", 302))
 	http.HandleFunc("/ws", wsHandler)
 	if err := http.ListenAndServe(*addr, nil); err != nil {
 		fmt.Printf("Error trying to bind to port: %v, so exiting...", err)
@@ -217,101 +213,3 @@ func externalIP() (string, error) {
 	}
 	return "", errors.New("are you connected to the network?")
 }
-
-var homeTemplate = template.Must(template.New("home").Parse(homeTemplateHtml))
-
-// If you navigate to this server's homepage, you'll get this HTML
-// so you can directly interact with the serial port server
-const homeTemplateHtml = `<!DOCTYPE html>
-<html>
-<head>
-<title>Serial Port Example</title>
-<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.4.2/jquery.min.js"></script>
-<script type="text/javascript">
-    $(function() {
-
-    var conn;
-    var msg = $("#msg");
-    var log = $("#log");
-
-    function appendLog(msg) {
-        var d = log[0]
-        var doScroll = d.scrollTop == d.scrollHeight - d.clientHeight;
-        msg.appendTo(log)
-        if (doScroll) {
-            d.scrollTop = d.scrollHeight - d.clientHeight;
-        }
-    }
-
-    $("#form").submit(function() {
-        if (!conn) {
-            return false;
-        }
-        if (!msg.val()) {
-            return false;
-        }
-        conn.send(msg.val() + "\n");
-        msg.val("");
-        return false
-    });
-
-    if (window["WebSocket"]) {
-        conn = new WebSocket("ws://{{$}}/ws");
-        conn.onclose = function(evt) {
-            appendLog($("<div><b>Connection closed.</b></div>"))
-        }
-        conn.onmessage = function(evt) {
-            appendLog($("<div/>").text(evt.data))
-        }
-    } else {
-        appendLog($("<div><b>Your browser does not support WebSockets.</b></div>"))
-    }
-    });
-</script>
-<style type="text/css">
-html {
-    overflow: hidden;
-}
-
-body {
-    overflow: hidden;
-    padding: 0;
-    margin: 0;
-    width: 100%;
-    height: 100%;
-    background: gray;
-}
-
-#log {
-    background: white;
-    margin: 0;
-    padding: 0.5em 0.5em 0.5em 0.5em;
-    position: absolute;
-    top: 0.5em;
-    left: 0.5em;
-    right: 0.5em;
-    bottom: 3em;
-    overflow: auto;
-}
-
-#form {
-    padding: 0 0.5em 0 0.5em;
-    margin: 0;
-    position: absolute;
-    bottom: 1em;
-    left: 0px;
-    width: 100%;
-    overflow: hidden;
-}
-
-</style>
-</head>
-<body>
-<div id="log"></div>
-<form id="form">
-    <input type="submit" value="Send" />
-    <input type="text" id="msg" size="64"/>
-</form>
-</body>
-</html>
-`
